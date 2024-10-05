db01dc8d340f4ae56a4e2bbf9d850aaa1bdac181
hrishim <hrishim@gmail.com>
2024-10-05 17:56:44 +0530

    Initial Commit

---
+++ DeleteConfirmation/DeleteConfirmationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteConfirmationApp.swift
+//  DeleteConfirmation
+//
+//  Created by Hrishikesh on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DeleteConfirmationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeleteConfirmationTests/DeleteConfirmationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteConfirmationTests.swift
+//  DeleteConfirmationTests
+//
+//  Created by Hrishikesh on 05/10/24.
+//
+
+import Testing
+@testable import DeleteConfirmation
+
+struct DeleteConfirmationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
